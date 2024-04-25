a3a0ea5dafea2a7d699d5d3a6ed1ab381095eb29
yourboykungu <badusvanmark@gmail.com>
2024-04-25 17:21:34 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("connected to mongodb");
+}).catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
 app.listen(3000,(req,res) => {
-    console.log('listening on port 3000')
+    console.log('listening on port 3000');
 });
