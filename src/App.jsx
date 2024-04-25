d35405f22e6d7cfa401ba9f5a6920dbcd547ead1
yourboykungu <badusvanmark@gmail.com>
2024-04-25 10:47:44 +0300

    creating paths na routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import Profile from './pages/profile';
+import SignIn from './pages/signIn';
+import SignUp from './pages/signUp';
+
 
 export default function App() {
   return (
-    <div>App</div>
+   <BrowserRouter>
+   <Routes>
+      
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-up" element={<SignUp />} />
+    
+   </Routes>
+   </BrowserRouter>
+    
   )
 }
